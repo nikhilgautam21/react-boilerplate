ab8ecd887d5a2b74d0bc24110a33f469ac9c7619
Nikhil Gautam <nikhilnn@blah.com>
2020-06-22 10:25:25 +0530

    prepared folder structure, added essential npm packages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers/index';
+import { BrowserRouter } from "react-router-dom";
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
